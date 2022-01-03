0bf221bfadfb58e7ca7b86289aa8a1344827395c
thkernel <a.dembele@upcase.net>
2022-01-03 12:11:16 +0000

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,12 +52,12 @@ set :repo_url, "https://github.com/thkernel/teledocplus.git"
 
 # For rbenvu usage
 # config/deploy.rb
-#set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
-#set :rbenv_ruby, '2.7.5'
+set :rbenv_type, :user # or :system, or :fullstaq (for Fullstaq Ruby), depends on your rbenv setup
+set :rbenv_ruby, '2.7.5'
 
 # in case you want to set ruby version from the file:
 # set :rbenv_ruby, File.read('.ruby-version').strip
 
-#set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-#set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-#set :rbenv_roles, :all # default value
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all # default value
